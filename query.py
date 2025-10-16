453620c07482ae9bc9d079710f255cf70833b417
bharath-kumar-m <bharath-kumar.m@hpe.com>
2025-10-16 00:21:36 -0700

    some

--- query.py
+++ query.py
@@ -5,7 +5,7 @@ import pickle
 import numpy as np
 import sys
 import subprocess
-
+import requests
 # Load model offline
 model = SentenceTransformer("models/all-MiniLM-L6-v2")
 
@@ -19,37 +19,38 @@ if len(sys.argv) < 2:
     print("Usage: python query.py <query_string>")
     sys.exit(1)
 
-query = sys.argv[1]
-print("Query: ", query)
-query_vec = model.encode([query], convert_to_numpy=True)
+customer_query = sys.argv[1]
+print("Query: ", customer_query)
+query_vec = model.encode([customer_query], convert_to_numpy=True)
 
 # Retrieve top 3 relevant chunks
-k = 3
+k = 2
 D, I = index.search(query_vec.astype(np.float32), k)
 
-print("Top relevant chunks:")
-for idx in I[0]:
-    print("Chunk: ", chunks[idx])
+# print("Top relevant chunks:")
+# for idx in I[0]:
+    # print("Chunk: ", chunks[idx])
 rag_chunks = "\n".join([chunks[idx] for idx in I[0]])
 
 query = f"""
 You are an assistant that analyzes customer server details and the retrieved RAG data (selected from support documentation).
-
 Your tasks are:
-
 Identify the current Service Pack for ProLiant (SPP) bundle used by the customer.
 Review the RAG data chunks provided.
 Determine the best recommended SPP upgrade for the customer, based on compatibility and relevance.
-
 RAG Data:
 {rag_chunks}
+"Customer Data:
+{customer_query}
 """
 
 payload_dict = {
     "model": "llama3.2:3b",
     "prompt": query
 }
-command = f'curl -X POST --noproxy localhost -H "Content-Type: application/json" http://localhost:11435/api/generate -d {json.dumps(payload_dict)}'
+data=json.dumps(payload_dict)
+print("Model query: ",data)
+command = f'curl -X POST --noproxy localhost -H "Content-Type: application/json" http://localhost:11435/api/generate -d @{temp_file}'
 # Execute the curl command
 result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
